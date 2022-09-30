a8d57e9a13f9dd12d137a6a0bf1e84a5f5e30bdb
Gabriela Garcia <g.garciac@uniandes.edu.co>
2022-09-30 09:06:50 -0500

    prueba cafe

--- src/cafe/cafe.service.spec.ts
+++ src/cafe/cafe.service.spec.ts
@@ -5,11 +5,13 @@ import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { faker } from '@faker-js/faker';
+import { TiendaEntity } from 'src/tienda/tienda.entity';
 
 describe('CafeService', () => {
   let service: CafeService;
   let repository: Repository<CafeEntity>;
   let cafeList: CafeEntity[];
+  let tiendasList: TiendaEntity[];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -41,4 +43,43 @@ describe('CafeService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('create debe crear un cafe', async () => {
+    const cafe: CafeEntity = {
+      id: '',
+      nombre: faker.name.fullName(),
+      descripcion: faker.lorem.sentence(),
+      precio : faker.datatype.number(),
+      tiendas: tiendasList,
+    };
+
+    const newcafe: CafeEntity = await service.create(cafe);
+    expect(cafe).not.toBeNull();
+
+    const storedCafe: CafeEntity = await repository.findOne({
+      where: { id: cafe.id },
+    });
+    expect(storedCafe).not.toBeNull();
+    expect(storedCafe.nombre).toEqual(newcafe.nombre);
+    expect(storedCafe.descripcion).toEqual(newcafe.descripcion);
+    expect(storedCafe.precio).toEqual(newcafe.precio);
+    expect(storedCafe.tiendas).toEqual(newcafe.tiendas);
+  });
+
+  it('create debe lanzar una excepción por un cafe inválido', async () => {
+    const cafe: CafeEntity = {
+      id: '',
+      nombre: faker.name.fullName(),
+      descripcion: faker.lorem.sentence(),
+      precio : faker.datatype.number(),
+      tiendas: tiendasList,      
+    };
+
+    await expect(() => service.create(cafe)).rejects.toHaveProperty(
+      'message',
+      'El precio del cafe debe ser un valor positivo ',
+    );
+  });
+
+
 });
